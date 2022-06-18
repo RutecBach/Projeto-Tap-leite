4c9bbfcc11694b0ff7743312d31eecdecea9b471
Rute Cristina Bach <53106245+RutecBach@users.noreply.github.com>
2022-06-17 23:02:52 -0300

    Add files via upload

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'directives-app'`, () => {
+  it(`should have as title 'lifecycle'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('directives-app');
+    expect(app.title).toEqual('lifecycle');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to directives-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lifecycle!');
   });
 });
